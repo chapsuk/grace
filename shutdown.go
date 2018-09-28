ea0e7796e79cc700b7bbfcc62cef242837ad348c
Yan Mikhailov <yanmhlv@users.noreply.github.com>
2018-09-28 12:22:55 +0300

    Exit from goroutine if context was cancelled (#2)

--- shutdown.go
+++ shutdown.go
@@ -14,8 +14,12 @@ func ShutdownContext(c context.Context) context.Context {
 	go func() {
 		ch := make(chan os.Signal, 1)
 		signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
-		<-ch
-		cancel()
+		select {
+		case <-ctx.Done():
+			return
+		case <-ch:
+			cancel()
+		}
 	}()
 	return ctx
 }
